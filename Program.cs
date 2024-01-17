93b5be56a79436e2957459e23c20ae26a9fc33f5
Salvador <sarevalotamayo@student.rtc.edu>
2024-01-16 16:52:12 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GA_ConsoleReadLine_Salvador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, " + userName + "!");
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("Ah, " + favoriteColor + " is a great color!");
+            Console.WriteLine(userName + ", who likes " + favoriteColor + ", Welcome to the Program!");
+
+        }
+    }
+}
